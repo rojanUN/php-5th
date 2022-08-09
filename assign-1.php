b20474448ab46eb8a7c499adba4ec8bfb35e1402
rojanUN <101305319+rojanUN@users.noreply.github.com>
2022-08-09 20:19:14 +0545

    Add files via upload

---
+++ assign-1.php
@@ -0,0 +1,14 @@
+!<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>PHP1</title>
+  </head>
+  <body>
+    <?php
+         define("name","joeFormFortnite<br>");
+         echo name;
+         echo "HELO WRLDDD";
+         ?>
+  </body>
+</html>
