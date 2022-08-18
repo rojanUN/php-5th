2ebe0576a22c285b6b733da59a4137dc1e48671d
rojanUN <101305319+rojanUN@users.noreply.github.com>
2022-08-18 21:21:49 +0545

    Add files via upload

---
+++ change.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Rojan Kirana Pasal</title>
+  </head>
+  <body>
+    <form  action="change.php" method="get">
+        Name: <input type="text" name="name"><br>
+        Attendance: <input type="number" name="att" placeholder="<100%"><br>
+        Hygiene rating: <input type="number" name="hy" placeholder="<100%"><br>
+        rating: <input type="number" name="rating" placeholder="upto 5"><br>
+        <input type="submit">
+    </form>
+
+    <?php
+        $name = $_GET["name"];
+        $attendance = $_GET["att"];
+        $hygiene = $_GET["hy"];
+        $rating = $_GET["rating"];
+
+        function evaluate($name, $attendance, $hygiene, $rating){
+            if (($attendance<80 && $hygiene<50) || ($hygiene<50 && $rating<2)) {
+              echo "$name, you have been fired from rojan kirana Pasal";
+            }
+            else {
+              echo "$name, you are not fired yet";
+            }
+        }
+        evaluate($name, $attendance, $hygiene, $rating);
+      ?>
+
+  </body>
+</html>
